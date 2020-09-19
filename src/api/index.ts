aefd4917e53cd13378e233065e2dbc9dbb2a5a12
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-09-19 16:26:28 +0200

    Remove comment

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,4 @@
 export async function getSaldo(cardNumber: string): Promise<string> {
-  // const url = 'http://localhost:63218/?cardNo=';
   const url = 'https://mhx.be/beerschot-saldo-checker/api?cardNo=';
   return await (await fetch(`${url}${cardNumber}`)).json();
 }
