c68419e7ebde02af4682086a054ee5feb7b012ba
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-09-23 22:10:24 +0200

    Enable api on localhost for testing purposes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,9 @@
 export async function getSaldo(cardNumber: string): Promise<string> {
   let url = `/api/?cardNo=`;
-  if (process.env.NODE_ENV === 'development') {
+  if (
+    process.env.NODE_ENV === 'development' ||
+    location.hostname === 'localhost'
+  ) {
     url = `http://localhost:57888${url}`;
   }
   return await (await fetch(`${url}${cardNumber}`)).json();
